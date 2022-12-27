0dbe1133150337b6443953f00ac0d992b25828f8
lorenzo <lorenzo@vectorcr.com>
2022-12-27 17:56:32 -0600

    Checkin
    Style, Container, Structure

---
+++ 2-365/notetaker/src/Components/NoteComponents/Notes.js
@@ -0,0 +1,15 @@
+import { React, useState, useEffect } from "react";
+import "../css/Note.css";
+import Note from "./Note";
+
+function Notes() {
+    return (
+        <div className="notes">
+            <Note />
+            <Note />
+            <Note />
+        </div>
+    );
+}
+
+export default Notes;
